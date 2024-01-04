87b61f2d516c50e73580aa34a4cdbba81c919d23
maskelog <blitzmaske@gmail.com>
2024-01-04 23:04:08 +0900

    Card icon 추가

--- toonweb/lib/main.dart
+++ toonweb/lib/main.dart
@@ -120,6 +120,7 @@ class App extends StatelessWidget {
                   height: 20,
                 ),
                 Container(
+                  clipBehavior: Clip.hardEdge,
                   decoration: BoxDecoration(
                     color: const Color.fromARGB(255, 58, 58, 58),
                     borderRadius: BorderRadius.circular(20),
@@ -127,6 +128,7 @@ class App extends StatelessWidget {
                   child: Padding(
                     padding: const EdgeInsets.all(30),
                     child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
@@ -165,6 +167,17 @@ class App extends StatelessWidget {
                             ),
                           ],
                         ),
+                        Transform.scale(
+                          scale: 2.2,
+                          child: Transform.translate(
+                            offset: const Offset(3, 12),
+                            child: const Icon(
+                              Icons.euro_rounded,
+                              color: Colors.white,
+                              size: 88,
+                            ),
+                          ),
+                        )
                       ],
                     ),
                   ),
