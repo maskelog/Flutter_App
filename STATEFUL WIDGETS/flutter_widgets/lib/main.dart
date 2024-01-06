c2b1b94bc6468dc58e5a591168de21afe80ecd56
maskelog <blitzmaske@gmail.com>
2024-01-06 23:13:48 +0900

    STATEFUL WIDGETS

---
+++ STATEFUL WIDGETS/flutter_widgets/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MaterialApp(
+        home: Scaffold(
+          backgroundColor: const Color(0xFFF4EDDB),
+          body: Container(),
+        ),
+      ),
+    );
+  }
+}
