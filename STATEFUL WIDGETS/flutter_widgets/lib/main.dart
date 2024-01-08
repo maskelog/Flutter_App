b3e10d718a38f5bec30cfecc2c4b16421909e492
maskelog <blitzmaske@gmail.com>
2024-01-09 01:06:44 +0900

    Widget Lifecycle

--- STATEFUL WIDGETS/flutter_widgets/lib/main.dart
+++ STATEFUL WIDGETS/flutter_widgets/lib/main.dart
@@ -12,26 +12,36 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  bool showTitle = true;
+
+  void toggleTitle() {
+    setState(() {
+      showTitle = !showTitle;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MaterialApp(
-        theme: ThemeData(
-          textTheme: const TextTheme(
-            titleLarge: TextStyle(
-              color: Colors.red,
-            ),
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            color: Colors.red,
           ),
         ),
-        home: const Scaffold(
-          backgroundColor: Color(0xFFF4EDDB),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                MyLargeTitle(),
-              ],
-            ),
+      ),
+      home: Scaffold(
+        backgroundColor: const Color(0xFFF4EDDB),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              showTitle ? const MyLargeTitle() : const Text('nothing'),
+              IconButton(
+                onPressed: toggleTitle,
+                icon: const Icon(Icons.remove_red_eye),
+              )
+            ],
           ),
         ),
       ),
@@ -39,19 +49,34 @@ class _AppState extends State<App> {
   }
 }
 
-class MyLargeTitle extends StatelessWidget {
+class MyLargeTitle extends StatefulWidget {
   const MyLargeTitle({
     super.key,
   });
 
   @override
+  State<MyLargeTitle> createState() => _MyLargeTitleState();
+}
+
+class _MyLargeTitleState extends State<MyLargeTitle> {
+  @override
+  void initState() {
+    super.initState();
+    print('initState!');
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    print('dispose!');
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print('build!');
     return Text(
       'My Large Title',
-      style: TextStyle(
-        fontSize: 30,
-        color: Theme.of(context).textTheme.titleLarge?.color,
-      ),
+      style: Theme.of(context).textTheme.titleLarge?.copyWith(fontSize: 30),
     );
   }
 }
