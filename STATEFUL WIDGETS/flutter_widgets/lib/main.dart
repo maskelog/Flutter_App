1f0939817c8daa8839325ea9639d7caba792aa3b
maskelog <blitzmaske@gmail.com>
2024-01-09 00:10:18 +0900

    BuildContext

--- STATEFUL WIDGETS/flutter_widgets/lib/main.dart
+++ STATEFUL WIDGETS/flutter_widgets/lib/main.dart
@@ -12,54 +12,45 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  List<int> numbers = [];
-
-  void onClicked() {
-    setState(() {
-      numbers.add(numbers.length);
-    });
-  }
-
-  void onMinus() {
-    setState(() {
-      if (numbers.isNotEmpty) {
-        numbers.removeLast();
-      }
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: MaterialApp(
-        home: Scaffold(
-          backgroundColor: const Color(0xFFF4EDDB),
+        theme: ThemeData(
+          textTheme: const TextTheme(
+            titleLarge: TextStyle(
+              color: Colors.red,
+            ),
+          ),
+        ),
+        home: const Scaffold(
+          backgroundColor: Color(0xFFF4EDDB),
           body: Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const Text(
-                  'Click Count',
-                  style: TextStyle(fontSize: 30),
-                ),
-                for (var n in numbers) Text('$n'),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    IconButton(
-                        iconSize: 40,
-                        onPressed: onClicked,
-                        icon: const Icon(Icons.add_box_rounded)),
-                    IconButton(
-                        iconSize: 40,
-                        onPressed: onMinus,
-                        icon: const Icon(Icons.remove_circle_rounded)),
-                  ],
-                )
+                MyLargeTitle(),
               ],
             ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class MyLargeTitle extends StatelessWidget {
+  const MyLargeTitle({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'My Large Title',
+      style: TextStyle(
+        fontSize: 30,
+        color: Theme.of(context).textTheme.titleLarge?.color,
       ),
     );
   }
