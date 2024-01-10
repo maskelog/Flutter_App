3f8d83011fcd5b667e27e0d6937f695323f4c2d2
maskelog <blitzmaske@gmail.com>
2024-01-10 23:12:52 +0900

    icon 추가 25:00 Theme 색상 사용

--- pomodoro/pomodoro/lib/screens/home_screen.dart
+++ pomodoro/pomodoro/lib/screens/home_screen.dart
@@ -18,10 +18,10 @@ class _HomeScreenState extends State<HomeScreen> {
             flex: 1,
             child: Container(
               alignment: Alignment.bottomCenter,
-              child: const Text(
+              child: Text(
                 '25:00',
                 style: TextStyle(
-                  color: Colors.white,
+                  color: Theme.of(context).cardColor,
                   fontSize: 80,
                   fontWeight: FontWeight.w600,
                 ),
@@ -30,7 +30,16 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
           Flexible(
             flex: 3,
-            child: Container(),
+            child: Center(
+              child: IconButton(
+                iconSize: 98,
+                color: Theme.of(context).cardColor,
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.play_circle_outlined,
+                ),
+              ),
+            ),
           ),
           Flexible(
             flex: 1,
