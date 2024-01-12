857213db14d92d4704101a32f76dbb31b56f65b7
maskelog <blitzmaske@gmail.com>
2024-01-12 22:20:23 +0900

    리셋버튼  추가

--- pomodoro/pomodoro/lib/screens/home_screen.dart
+++ pomodoro/pomodoro/lib/screens/home_screen.dart
@@ -51,6 +51,17 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
+  void onReset() {
+    if (timer.isActive) {
+      timer.cancel();
+    }
+    setState(() {
+      totalSeconds = twentyFiveMinutes;
+      isRunning = false;
+      totalPomodoros = 0;
+    });
+  }
+
   String format(int seconds) {
     var duration = Duration(seconds: seconds);
     return duration.toString().split('.').first.substring(2, 7);
@@ -78,13 +89,25 @@ class _HomeScreenState extends State<HomeScreen> {
           Flexible(
             flex: 3,
             child: Center(
-              child: IconButton(
-                iconSize: 120,
-                color: Theme.of(context).cardColor,
-                onPressed: isRunning ? onPausePressed : onStartPressed,
-                icon: Icon(isRunning
-                    ? Icons.pause_circle_outline
-                    : Icons.play_circle_outline),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(
+                    iconSize: 120,
+                    color: Theme.of(context).cardColor,
+                    onPressed: isRunning ? onPausePressed : onStartPressed,
+                    icon: Icon(isRunning
+                        ? Icons.pause_circle_outline
+                        : Icons.play_circle_outline),
+                  ),
+                  IconButton(
+                    iconSize: 30,
+                    color: Theme.of(context).cardColor,
+                    onPressed: onReset,
+                    icon: const Icon(Icons.refresh_outlined),
+                  ),
+                ],
               ),
             ),
           ),
